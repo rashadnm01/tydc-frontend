678a34d85f72c0be60ef78f9b5cd17dbf3a7787f
Rashad M <rashadnm01@gmail.com>
2022-07-02 00:53:12 -0500

    Updated mobile styling, added a login page

--- src/components/header/login-signup-text/index.js
+++ src/components/header/login-signup-text/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./_login-signup-text.scss";
 export const LoginSignupText = () => {
+  const navigate = useNavigate();
   return (
     <div className="login-signup-text">
-      Have an account? <br /> <u>Login</u>, or <u>sign up</u>.
+      Have an account? <br />{" "}
+      <u onClick={() => navigate("/login", { replace: true })}>Login</u>, or{" "}
+      <u onClick={() => window.history.replaceState(null, "", "/signup")}>
+        sign up
+      </u>
+      .
     </div>
   );
 };
--- src/components/header/nav/index.js
+++ src/components/header/nav/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./_nav.scss";
 export const Nav = () => {
+  let navigate = useNavigate();
   return (
     <div className="nav">
-      <div className="list-item">Home</div>
+      <div
+        className="list-item"
+        onClick={() => navigate("/", { replace: true })}
+      >
+        Home
+      </div>
       <div className="list-item">Hire Our Developers</div>
       <div className="list-item">About Us</div>
       <div className="list-item">Leave Us Feedback</div>
